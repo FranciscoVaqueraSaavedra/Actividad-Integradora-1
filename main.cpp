d91c3291ce9175e451f073d68001c686c2d5108b
FranciscoVaqueraSaavedra <141375999+FranciscoVaqueraSaavedra@users.noreply.github.com>
2025-05-19 20:22:06 -0600

    Hola5

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+
+/* This source file must have a .cpp extension so that all C++ compilers
+   recognize the extension without flags.  Borland does not know .cxx for
+   example.  */
+
 #include <iostream>
 #include <fstream>
 #include <string>
