f8c4cfc0ecf5d6e514475ae18424db591f869a9c
FranciscoVaqueraSaavedra <141375999+FranciscoVaqueraSaavedra@users.noreply.github.com>
2025-05-21 01:10:17 -0600

    Hola2

--- substring.h
+++ substring.h
@@ -11,6 +11,19 @@
 #include <string>
 #include <utility>
 
+/**
+ * @brief Encuentra la subcadena común más larga entre dos textos.
+ *
+ * Esta función calcula el inicio y la longitud de la subcadena más larga
+ * que aparece en ambos textos proporcionados.
+ *
+ * @param text1 Primer texto.
+ * @param text2 Segundo texto.
+ * @return std::pair<int, int> Un par con:
+ *         - first: índice inicial en text1 donde comienza la subcadena.
+ *         - second: longitud de la subcadena común más larga.
+ */
+
 std::pair<int, int> longest_common_substring(const std::string &text1, const std::string &text2);
 
 #endif // SUBSTRING_H
